14175c12c6f35defd746d683c86b3ed2fdd15e00
mar-ia-csv <maria.femenias.hermida@gmail.com>
2025-04-04 23:41:08 +0200

    feature: reading time extracted

--- source/scraping/core/utils.py
+++ source/scraping/core/utils.py
@@ -35,7 +35,7 @@ def extract_article_metadata(article):
             date = datetime.strptime(f"{raw_date} {datetime.now().year}",
                                      "%b %d %Y").strftime("%Y-%m-%d")
             
-    # Time to read (-1 if not found)
+    # Time to read (-1 if not found) 
     read_time_el = article.query_selector("div.crayons-story__save small.crayons-story__tertiary")
     read_time = int(read_time_el.inner_text().strip().split()[0]) if read_time_el else -1
 
